c30010a7b6f575af514818def3b82e67e3e163ef
Eduardo Saatkamp <eduardosaatkamp@gmail.com>
2022-04-13 18:20:39 -0300

    Add files via upload

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -10,6 +10,10 @@ public class primeiroPrograma {
     public static void main(String[] args) {
         System.out.println("Olá Mundo Dev!");
         System.out.println("Este é meu primeiro programa!");
+        System.out.println(".﹎ ┈ ┈ ┈ ﹎");
+        System.out.println("﹎┈  ●  o  .﹎ ﹎");
+        System.out.println("┈ ┈ /█\\/▓\\ ﹎ ┈");
+        System.out.println("^^^^^^^^^^^^^^^^");
     }//fim do método
 }//fim da classe
 
