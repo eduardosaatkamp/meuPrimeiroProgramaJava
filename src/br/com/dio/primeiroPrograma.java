6ec1565056aa575116cfa596011dbcfcaa45545e
Eduardo Saatkamp <eduardosaatkamp@gmail.com>
2022-04-13 17:37:56 -0300

    Add files via upload

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,15 @@
+/**
+ *Programa que imprime em tela Hello World!
+ *@author Eduardo S.
+ */
+
+package br.com.dio;
+
+public class primeiroPrograma {
+    /* Método principal da classe*/
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo Dev!");
+        System.out.println("Este é meu primeiro programa!");
+    }//fim do método
+}//fim da classe
+
